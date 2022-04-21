0e079baa38ad1957bfe432694b214d76f1d86058
Douglas Balbino <103530462+douglasGHub@users.noreply.github.com>
2022-04-21 10:16:46 -0300

    IMPRIMIR TODOS OS NÚMEROS ÍMPARES

---
+++ src/forImpar.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class forImpar
+{
+public static void main(String[] args) {
+Scanner entrada = new Scanner (System.in);
+int x=0;
+int n=0;
+System.out.println("Digite o Número");
+n = entrada.nextInt();
+for (x=1; x<=n; x+=2){
+            System.out.println(x);
+   
+}
+}
+
+
+}
+
